a4665361c58ade46af2d585ebe1a26c62e385133
HarvySantiagoR <71794231+harvyRosero@users.noreply.github.com>
2021-10-04 13:05:18 -0500

    pop ups

--- models/conexion_sql.php
+++ models/conexion_sql.php
@@ -1,7 +1,7 @@
 <?php 
 
     // CONEXION SQL 
-    $mysqli = new mysqli("sql205.epizy.com", "epiz_29914276", "SgplCipyC9", "epiz_29914276_pruebaeml");
+    $mysqli = new mysqli("localhost", "root", "root", "empresa2");
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
